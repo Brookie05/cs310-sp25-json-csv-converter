3f13c250414bca00bd5e7ef034fcad21bb25f950
Brooklyn Leonard <bleonard2@stu.jsu.edu>
2025-02-09 21:58:49 -0600

    Completed Individual Project 1 !

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -2,6 +2,9 @@ package edu.jsu.mcis.cs310;
 
 import com.github.cliftonlabs.json_simple.*;
 import com.opencsv.*;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.List;
 
 public class Converter {
     
@@ -71,40 +74,113 @@ public class Converter {
         
     */
     
+    // Utility method for CSV parsing
+    private static CSVReader createCsvReader(String csvString) {
+        CSVParser cParse = new CSVParserBuilder().withSeparator(',').build();
+        return new CSVReaderBuilder(new StringReader(csvString))
+                .withCSVParser(cParse)
+                .build();
+    }
+    
+    // Utility method for type conversion
+    private static Object convertDataType(String[] columnHeadings, int index, String value) {
+        if (columnHeadings[index].equals("Season") || columnHeadings[index].equals("Episode")) {
+            return Integer.parseInt(value);
+        }
+        return value;
+    }
+    
     @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
-        
         String result = "{}"; // default return value; replace later!
         
         try {
-        
-            // INSERT YOUR CODE HERE
-            
+            // Reading all the CSV data that is given
+            CSVReader cReader = createCsvReader(csvString);
+            List<String[]> csvData = cReader.readAll();
+            // Taking the column headings from the front row
+            String[] columnHeadings = csvData.get(0);
+            // Starting the necessary JSON structures
+            JsonObject json = new JsonObject();
+            JsonArray producedNums = new JsonArray();
+            JsonArray data1 = new JsonArray();
+            // Processing the rows except for the header row
+            for (int i = 1; i < csvData.size(); i++) {
+                String[] row = csvData.get(i);
+                producedNums.add(row[0]);
+                // Making a JSON array for current row's data
+                JsonArray rowData = new JsonArray();
+                for (int j = 1; j < row.length; j++) {
+                    rowData.add(convertDataType(columnHeadings, j, row[j]));
+                }
+                // Adding the row data to the Data array made
+                data1.add(rowData);
+            }
+            // Adding the JSON structures to the main JSON object created
+            json.put("ProdNums", producedNums);
+            json.put("ColHeadings", columnHeadings);
+            json.put("Data", data1);
+            // Converting the JSON object to a string value
+            result = Jsoner.serialize(json);
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         
         return result.trim();
-        
+    }
+    
+    // Utility method for formatting episode number
+    private static String formatEpisodeNumber(String episode) {
+        return String.format("%02d", Integer.parseInt(episode));
     }
     
     @SuppressWarnings("unchecked")
     public static String jsonToCsv(String jsonString) {
-        
         String result = ""; // default return value; replace later!
         
         try {
-            
-            // INSERT YOUR CODE HERE
-            
+            // Parsing the JSON string so it can go into a JsonObject
+            JsonObject json = Jsoner.deserialize(jsonString, new JsonObject());
+            // Reading the column heading and Produced Numbers, as well as the Data
+            JsonArray columnHeadings = (JsonArray) json.get("ColHeadings");
+            JsonArray producedNums = (JsonArray) json.get("ProdNums");
+            JsonArray data2 = (JsonArray) json.get("Data");
+            // Making a CSV writer
+            StringWriter cWriter = new StringWriter();
+            CSVWriter csvWriter = new CSVWriter(cWriter);
+            // Writing the new header row
+            String[] hRow = new String[columnHeadings.size()];
+            for (int i = 0; i < columnHeadings.size(); i++) {
+                hRow[i] = columnHeadings.getString(i);
+            }
+            csvWriter.writeNext(hRow);
+            // Writing each row that is found
+            for (int i = 0; i < data2.size(); i++) {
+                JsonArray rData = (JsonArray) data2.get(i);
+                String[] Row = new String[columnHeadings.size()];
+                Row[0] = producedNums.getString(i);
+                // Adding the rest of the data needed
+                for (int j = 0; j < rData.size(); j++) {
+                    String columnName = columnHeadings.get(j + 1).toString();
+                    
+                    if (columnName.equals("Episode")) {
+                        // Making sure that there is only 2 digits in episode
+                        Row[j + 1] = formatEpisodeNumber(rData.get(j).toString());
+                    } else if (columnName.equals("Season")) {
+                        Row[j + 1] = rData.get(j).toString();
+                    } else {
+                        Row[j + 1] = rData.get(j).toString();
+                    }
+                }
+                csvWriter.writeNext(Row);
+            }
+
+            result = cWriter.toString();
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-        
         return result.trim();
-        
     }
-    
 }
